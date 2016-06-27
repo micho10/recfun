55053c38260108705e4851973bbafb54ae983dd3
micho <gatin13@yahoo.com>
2016-06-27 14:37:56 +0900

    Exercise 2 completed

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -8,21 +8,36 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("Parenthesis balance")
+
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
+  def pascal(c: Int, r: Int): Int =
     if (c < 0 || c > r) 0
     else if (r == 0 || c == r) 1
     else pascal(c, r-1) + pascal(c-1, r-1)
-  }
+
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def innerBalance(count: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) count == 0
+      else if (chars.head != '(' && chars.head != ')')
+        innerBalance(count, chars.tail)
+      else {
+        val aux = if (chars.head == '(') count + 1 else count - 1
+        aux >= 0 && innerBalance(aux, chars.tail)
+      }
+    }
+
+    innerBalance(0, chars)
+  }
   
   /**
    * Exercise 3
